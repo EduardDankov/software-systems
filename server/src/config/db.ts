6b85a58c0026a8f8e7bc90c56e74e78a4a66dc48
Eduard Dankov <eduard.dankov@gmail.com>
2023-10-22 14:23:02 +0300

    Replaced PostgreSQL Client with Pool.

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,19 +1,15 @@
 import dotenv from "dotenv";
 
-import {Client} from "pg";
+import {Pool} from "pg";
 
 dotenv.config();
 
-const db = new Client({
+const db = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-void db.connect()
-  .catch((err) => {
-    console.error(`⚠️ [Database] ${err}`);
-  });
 
 export { db };
