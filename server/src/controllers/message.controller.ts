5b66f725b711faf5540bb20ac97acf40f130c6d0
Eduard Dankov <eduard.dankov@gmail.com>
2023-11-13 17:48:24 +0200

    Changed output format to JSON

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -4,7 +4,7 @@ import {db} from "../config/db";
 
 async function getCount(req: Request, res: Response) {
   const dbRes: QueryResult = await db.query('SELECT COUNT(*) FROM messages');
-  res.send(dbRes.rows[0].count);
+  res.status(200).json(dbRes.rows);
 }
 
 export {
