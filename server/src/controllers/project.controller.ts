95fd9c2f4a282613ab7b7d6fffd874fa848082fd
Eduard Dankov <eduard.dankov@gmail.com>
2023-11-29 11:56:20 +0200

    Reverted request changes

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -54,17 +54,12 @@ async function updateProjectData(req: Request, res: Response) {
 }
 
 async function deleteProjectData(req: Request, res: Response) {
-  const projectQueryString: string = `
+  const queryString: string = `
     DELETE FROM projects 
-    WHERE project_id=$1 
+    WHERE project_id=$1
     RETURNING project_id AS id
   `;
-  const tasksQueryString: string = `
-    DELETE FROM tasks 
-    WHERE project_id=$1
-  `;
-  const dbRes: QueryResult = await db.query(projectQueryString, [req.body.projectId]);
-  await db.query(tasksQueryString, [req.body.projectId]);
+  const dbRes: QueryResult = await db.query(queryString, [req.body.projectId]);
   res.status(200).json(dbRes.rows);
 }
 
