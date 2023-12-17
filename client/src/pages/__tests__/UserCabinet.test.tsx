42af1a7a99d07eb12018909a81e1d235e89e6d79
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 03:52:00 +0200

    Wrote tests for UserCabinet page

---
+++ client/src/pages/__tests__/UserCabinet.test.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+import {UserCabinet} from "../user/UserCabinet.tsx";
+import {BrowserRouter} from "react-router-dom";
+
+
+describe('Pages', () => {
+  describe('UserCabinet', () => {
+    it('should render UserCabinet page', () => {
+      render(
+        <BrowserRouter>
+          <UserCabinet />
+        </BrowserRouter>
+      );
+
+      // Should redirect to login page if unauthorized
+      const content: HTMLElement = screen.getByText(/Cabinet/);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
