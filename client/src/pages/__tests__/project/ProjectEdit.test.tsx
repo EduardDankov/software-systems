9547514f681926a32f5bc316f9170d0377825afd
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 19:38:04 +0200

    Wrote tests for ProjectEdit page

---
+++ client/src/pages/__tests__/project/ProjectEdit.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {ProjectEdit} from "../../project/details/edit/ProjectEdit.tsx";
+import {BrowserRouter} from "react-router-dom";
+
+
+describe('Pages', () => {
+  describe('ProjectEdit', () => {
+    it('should render ProjectEdit page', () => {
+      const testId: number = 1;
+      render(
+        <div data-testid={testId}>
+          <BrowserRouter>
+            <ProjectEdit />
+          </BrowserRouter>
+        </div>
+      );
+
+      const content: HTMLElement = screen.getByTestId(testId);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
