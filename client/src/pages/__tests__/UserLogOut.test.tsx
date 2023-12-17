3c9ed2e03538f272a32022b09f6b1d6bad6fb536
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 04:01:20 +0200

    Wrote tests for UserLogOut page

---
+++ client/src/pages/__tests__/UserLogOut.test.tsx
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+
+import {UserLogOut} from "../user/logout/UserLogOut.tsx";
+
+describe('Pages', () => {
+  describe('UserLogOut', () => {
+    it('should render UserLogOut page', () => {
+      render(
+        <BrowserRouter>
+          <UserLogOut />
+        </BrowserRouter>
+      );
+
+      const content: HTMLElement = screen.getByText(/You were logged out/);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
