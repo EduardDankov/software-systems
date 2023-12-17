7d53e381e3c0e22c6a52f0d0e26e0226074982af
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 03:14:09 +0200

    Wrote tests for ProjectTable

---
+++ client/src/components/__tests__/ProjectTable.test.tsx
@@ -0,0 +1,89 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {ProjectTable} from "../ProjectTable.tsx";
+import {Project} from "../../models/project.tsx";
+
+describe('Components', () => {
+  describe('ProjectTable', () => {
+    it('should render ProjectTable', () => {
+      const testId: number = 1;
+      const projectTableMock: Array<Project> = [
+        {
+          id: 1,
+          name: "software-systems",
+          description: "A test university project",
+          manager: {
+            id: 1,
+            username: "John Smith",
+            email: "john.smith@example.com"
+          },
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+        {
+          id: 2,
+          name: "thorium-os",
+          description: "A test OS project",
+          manager: {
+            id: 2,
+            username: "Emily Johnson",
+            email: "emily.johnson@example.com"
+          },
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+        {
+          id: 3,
+          name: "amber-pharmaceuticals",
+          description: "A test enterprise website project",
+          manager: {
+            id: 3,
+            username: "Sam Jackson",
+            email: "sam.jackson@example.com"
+          },
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+      ];
+
+      const projectTableOnClickMock = (): void => {};
+
+      render(
+        <table data-testid={testId}>
+          <tbody>
+          {
+            projectTableMock.map((project, index) =>
+              <ProjectTable
+                key={index}
+                projectData={project}
+                onClick={projectTableOnClickMock}
+              />
+            )
+          }
+          </tbody>
+        </table>
+      );
+
+      const content: Array<HTMLElement> = screen.getAllByTestId(testId);
+
+      content.forEach(element => expect(element).toBeInTheDocument());
+    });
+
+    it('should render ProjectTable.Header', () => {
+      const testId: number = 2;
+
+      render(
+        <table data-testid={testId}>
+          <thead>
+          <ProjectTable.Header />
+          </thead>
+        </table>
+      );
+
+      const element: HTMLElement = screen.getByTestId(testId);
+
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
