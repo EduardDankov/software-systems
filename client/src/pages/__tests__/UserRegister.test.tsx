58070d02bcbf7becfe66f122df561b75b208c4aa
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 03:56:28 +0200

    Wrote tests for UserRegister page

---
+++ client/src/pages/__tests__/UserRegister.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {UserRegister} from "../user/register/UserRegister.tsx";
+import {BrowserRouter} from "react-router-dom";
+
+
+describe('Pages', () => {
+  describe('UserRegister', () => {
+    it('should render UserRegister page', () => {
+      render(
+        <BrowserRouter>
+          <UserRegister />
+        </BrowserRouter>
+      );
+
+      const content: Array<HTMLElement> = screen.getAllByText(/Register/);
+
+      content.forEach(element => expect(element).toBeInTheDocument());
+      expect(content.length).toEqual(2); // Page title and submit button label
+    });
+  });
+});
