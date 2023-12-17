a10b7d114e58aa598799ab7ff39d5674e317f7df
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 19:42:11 +0200

    Wrote tests for ProjectDelete page

---
+++ client/src/pages/__tests__/project/ProjectDelete.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {ProjectDelete} from "../../project/details/delete/ProjectDelete.tsx";
+import {BrowserRouter} from "react-router-dom";
+
+
+describe('Pages', () => {
+  describe('ProjectDelete', () => {
+    it('should render ProjectDelete page', () => {
+      const testId: number = 1;
+      render(
+        <div data-testid={testId}>
+          <BrowserRouter>
+            <ProjectDelete />
+          </BrowserRouter>
+        </div>
+      );
+
+      const content: HTMLElement = screen.getByTestId(testId);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
