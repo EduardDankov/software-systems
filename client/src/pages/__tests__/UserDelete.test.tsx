f7eff46504f5dc79747e57e28a2b4e15243be43d
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 04:05:11 +0200

    Wrote tests for UserDelete page

---
+++ client/src/pages/__tests__/UserDelete.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {UserDelete} from "../user/delete/UserDelete.tsx";
+import {BrowserRouter} from "react-router-dom";
+
+
+describe('Pages', () => {
+  describe('UserDelete', () => {
+    it('should render UserDelete page', () => {
+      const testId: number = 1;
+
+      render(
+        <div data-testid={testId}>
+          <BrowserRouter>
+            <UserDelete />
+          </BrowserRouter>
+        </div>
+      );
+
+      const content: HTMLElement = screen.getByTestId(testId);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
