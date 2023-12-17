2db5cc7ecedfa1fac9f49d589995fb21b15aca3b
Eduard Dankov <eduard.dankov@gmail.com>
2023-12-17 03:48:09 +0200

    Wrote tests for NotFound page

---
+++ client/src/pages/__tests__/NotFound.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import {render, screen} from "@testing-library/react";
+
+import {NotFound} from "../error/NotFound.tsx";
+
+describe('Pages', () => {
+  describe('NotFound', () => {
+    it('should render NotFound page', () => {
+      render(<NotFound />);
+
+      const content: HTMLElement = screen.getByText(/404: The page was not found\./);
+
+      expect(content).toBeInTheDocument();
+    });
+  });
+});
